fd3140ec37ae1a31481bed4b352df2f8aab66dae
maxim.kasyanov <m.kasyanov@kagis.kz>
2017-01-28 12:47:30 +0600

    init commit

---
+++ src/main/java/com/gitsby/dynamicfield/config/DataConfig.java
@@ -0,0 +1,81 @@
+package com.gitsby.dynamicfield.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Kasyanov Maxim on 1/28/2017.
+ */
+@Configuration
+@PropertySource(name = "localProp", value = {"classpath:application.properties"})
+@MapperScan("com.gitsby.dynamicfield.model")
+public class DataConfig {
+
+    private static final String PACKAGE_MAPPER_SCAN = "com.gitsby.dynamicfield.model";
+    private final Environment environment;
+
+
+    @Autowired
+    public DataConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClass"));
+        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("schema.sql"));
+        populator.addScript(new ClassPathResource("data.sql"));
+        return populator;
+    }
+
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setTypeAliasesPackage(PACKAGE_MAPPER_SCAN);
+        return sessionFactory;
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory().getObject());
+    }
+}
---
+++ src/main/java/com/gitsby/dynamicfield/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.gitsby.dynamicfield.config;
+
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Kasyanov Maxim on 1/28/2017.
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.gitsby.dynamicfield.*")
+public class WebConfig  extends WebMvcConfigurerAdapter {
+
+        private static final String APPLICATION_PROPERTIES = "application.properties";
+
+        @Bean
+        public PropertiesFactoryBean mapper() {
+            PropertiesFactoryBean bean = new PropertiesFactoryBean();
+            bean.setLocation(new ClassPathResource(APPLICATION_PROPERTIES));
+            return bean;
+        }
+}
